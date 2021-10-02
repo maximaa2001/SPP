53aedce1d44bc6286d8e48bff135b5f7ee2545ca
Maksim <maximaa2001@gmail.com>
2021-10-03 02:58:28 +0300

    laba_1

---
+++ laba_1/ThreadPool/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPool
+{
+    delegate void TaskDelegate();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskQueue taskQueue = new TaskQueue(10);
+
+            taskQueue.EnqueueTask(TaskOne);
+            Thread.Sleep(100);
+
+            taskQueue.EnqueueTask(TaskTwo);
+            Thread.Sleep(100);
+
+            taskQueue.EnqueueTask(TaskThree);
+            Thread.Sleep(100);
+
+            taskQueue.EnqueueTask(TaskOne);
+            Thread.Sleep(100);
+
+            taskQueue.EnqueueTask(TaskTwo);
+            Thread.Sleep(100);
+
+            taskQueue.EnqueueTask(TaskThree);
+            Thread.Sleep(100);
+
+            taskQueue.EnqueueTask(TaskOne);
+            Thread.Sleep(100);
+
+            taskQueue.EnqueueTask(TaskTwo);
+            Thread.Sleep(100);
+
+            taskQueue.EnqueueTask(TaskThree);
+            Thread.Sleep(100);
+
+            Console.ReadLine();
+        }
+
+        private static void TaskOne()
+        {
+            Console.WriteLine($"TaskOne {Thread.CurrentThread.Name}");
+        }
+        private static void TaskTwo()
+        {
+            Console.WriteLine($"TaskTwo {Thread.CurrentThread.Name}");
+        }
+        private static void TaskThree()
+        {
+            Console.WriteLine($"TaskThree {Thread.CurrentThread.Name}");
+        }
+    }
+}
---
+++ laba_1/ThreadPool/TaskQueue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ThreadPool
+{
+    class TaskQueue
+    {
+        private Queue<Thread> threadPool;
+        private TaskDelegate taskDelegate;
+
+        public TaskQueue(int count)
+        {
+            threadPool = new Queue<Thread>();
+            InitPool(count);
+        }
+
+        private void InitPool(int count)
+        {
+            for(int i = 0; i < count; i++)
+            {
+                Thread thread = new Thread(Process);
+                thread.Name = $"Thread {i}";
+                thread.Start();
+                threadPool.Enqueue(thread);
+            }
+        }
+
+        public void EnqueueTask(TaskDelegate task)
+        {
+            lock (this)
+            {
+                taskDelegate += task;
+            }
+        }
+
+        private void Process()
+        {
+            while (true)
+            {
+                lock (this)
+                {
+                    if (taskDelegate != null)
+                    {
+                        taskDelegate();
+                        taskDelegate = null;
+                    }
+                }
+            }
+        }
+    }
+}
