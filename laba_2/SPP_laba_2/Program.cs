f0854706c9c3b2de3d132a8544285c0be8faa7a1
Maksim <maximaa2001@gmail.com>
2021-11-08 18:09:49 +0300

    laba_2

---
+++ laba_2/SPP_laba_2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+namespace SPP_laba_2
+{
+    delegate void TaskDelegate(string from, string to);
+
+    class Program
+    {
+
+        private static ThreadPool threadPool;
+        private static string source_path;
+        private static string aim_path;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите исходный каталог");
+            source_path = Console.ReadLine();
+
+            Console.WriteLine("Введите целевой каталог");
+            aim_path = Console.ReadLine();
+
+
+            if (Directory.Exists(source_path) && Directory.Exists(aim_path))
+            {
+                threadPool = new ThreadPool(10);
+                string[] files = Directory.GetFiles(source_path);
+
+                foreach (string file in files)
+                {
+                    threadPool.EnqueueTask(copyFile, new CopyFileInfo(file, aim_path));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Неверно введены каталоги");
+            }
+
+            while(threadPool.getTasks() != 0)
+            {
+
+            }
+            Console.WriteLine($"Скопировано {threadPool.count} файлов");
+
+
+            Console.ReadLine();
+        }
+
+        public static void copyFile(string from, string to)
+        {
+            File.Copy(from, to, true);
+            Console.WriteLine(Thread.CurrentThread.Name); 
+        }
+    }
+}
---
+++ laba_2/SPP_laba_2/ThreadPool.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SPP_laba_2
+{
+    class ThreadPool
+    {
+        private Queue<Thread> threadPool;
+        private Queue<CopyFileInfo> filesInfo;
+        private Queue<TaskDelegate> tasks;
+        public int count { get; set; }
+
+        public ThreadPool(int count)
+        {
+            threadPool = new Queue<Thread>();
+            filesInfo = new Queue<CopyFileInfo>();
+            tasks = new Queue<TaskDelegate>();
+            InitPool(count);
+        }
+
+        private void InitPool(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Thread thread = new Thread(Process);
+                thread.Name = $"Thread {i}";
+                thread.Start();
+                threadPool.Enqueue(thread);
+            }
+        }
+
+        public void EnqueueTask(TaskDelegate task, CopyFileInfo info)
+        {
+            lock (this)
+            {
+                tasks.Enqueue(task);
+                filesInfo.Enqueue(info);
+            }
+        }
+
+        public int getTasks()
+        {
+            return tasks.Count;
+        }
+
+        private void Process()
+        {
+            while (true)
+            {
+                lock (this)
+                {
+                    if (tasks.Count != 0)
+                    {
+                        CopyFileInfo info = filesInfo.Dequeue();
+                        TaskDelegate task = tasks.Peek();
+                        task(info.from, info.to);
+                        count++;
+                        tasks.Dequeue();
+                        Thread.Sleep(1);
+                    }
+                }
+            }
+        }
+    }
+}
