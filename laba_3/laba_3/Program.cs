84387c007174ccc093acb6c86b321aa6f5c19eac
Maksim <maximaa2001@gmail.com>
2021-11-10 01:36:20 +0300

    laba_3

---
+++ laba_3/laba_3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace laba_3
+{
+    class Program
+    {
+
+        private static Mutex mutex = new Mutex();
+        static void Main(string[] args)
+        {
+           
+            for(int i = 0; i < 10; i++)
+            {
+                Thread thread = new Thread(print);
+                thread.Start();
+            }
+            Console.ReadLine();
+        }
+
+        public static void print()
+        {
+            mutex.Lock();
+            Console.WriteLine("Start thread " + Thread.CurrentThread.ManagedThreadId);
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("thread " + Thread.CurrentThread.ManagedThreadId + " step " + i);
+            }
+            Console.WriteLine("Finish thread " + Thread.CurrentThread.ManagedThreadId);
+            mutex.Unlock();
+        }
+    }
+}
