0cf7e0504a849cf1e1388b11e8f3ccd41833f8ba
Maksim <maximaa2001@gmail.com>
2021-11-18 22:06:13 +0300

    laba_5

---
+++ laba_5/laba_5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+
+namespace laba_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь в dll файлу");
+            string path = Console.ReadLine();
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Неверный путь");
+                Console.ReadLine();
+                return;
+            }
+
+            string extension = Path.GetExtension(path);
+
+            if(extension.Equals(".dll") || extension.Equals(".exe"))
+            {
+                Assembly asm = Assembly.LoadFrom(path);
+                var types = asm.GetTypes().Where(type => type.IsPublic).OrderBy(type => type.Namespace).ThenBy(type => type.Name);
+                foreach (Type t in types)
+                {
+                    Console.WriteLine(t.FullName);
+                }
+            }
+            else
+            {
+                Console.WriteLine("У файла неверное расширение");
+            }
+            Console.ReadLine();
+        }
+    }
+}
