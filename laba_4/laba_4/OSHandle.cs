502f868ed7a43dab436a1fd15a70d89033b8ca4b
Maksim <maximaa2001@gmail.com>
2021-11-18 19:56:20 +0300

    laba_4

---
+++ laba_4/laba_4/OSHandle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace laba_4
+{
+    class OSHandle : IDisposable
+    {
+
+        [DllImport("Kernel32.dll")]
+        private static extern bool CloseHandle(IntPtr handle);
+
+        public IntPtr Handle { set; get; }
+       
+        public OSHandle(IntPtr Handle)
+        {
+            this.Handle = Handle;
+        }
+
+        ~OSHandle(){
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (Handle != IntPtr.Zero)
+            {
+                Console.WriteLine(CloseHandle(Handle));
+                Handle = IntPtr.Zero;
+                Console.WriteLine("Dispose with disposing = " + disposing + ", Handle = " + Handle);
+            }
+        }
+    }
+}
---
+++ laba_4/laba_4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
+
+namespace laba_4
+{
+    class Program
+    {
+        public const uint GENERIC_READ = 0x80000000;
+        public const uint OPEN_EXISTING = 3;
+        static void Main(string[] args)
+        {
+
+            SafeFileHandle file = CreateFile("D:/laba_4.txt", GENERIC_READ, 0, IntPtr.Zero, OPEN_EXISTING, 0, IntPtr.Zero);
+
+            using (OSHandle os = new OSHandle(file.DangerousGetHandle()))
+            {
+                Console.WriteLine(os.Handle);
+            }
+            Console.ReadLine();
+        }
+
+      [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+      static extern SafeFileHandle CreateFile(string lpFileName, uint dwDesiredAccess,
+      uint dwShareMode, IntPtr lpSecurityAttributes, uint dwCreationDisposition,
+      uint dwFlagsAndAttributes, IntPtr hTemplateFile);
+    }
+}
